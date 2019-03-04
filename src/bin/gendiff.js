4feb26affa41aeeb9ad4a733e312cfe1efd3fe48
Simp-lexx <imlexx007@gmail.com>
2019-03-05 01:51:11 +1000

    create gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+import program from 'commander';
+import { version } from '../../package.json';
+import genDiff from '..';
+
+program
+  .version(version)
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format', 'recursive')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) =>
+    console.log(genDiff(firstConfig, secondConfig, program.format)));
+
+program.parse(process.argv);
