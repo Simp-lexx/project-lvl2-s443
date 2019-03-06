94f02e210265a6345a6611f7fa93f02c695a86b4
Simp-lexx <46103626+Simp-lexx@users.noreply.github.com>
2019-03-06 16:40:35 +1000

    Update gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -10,5 +10,3 @@ program
   .option('-f, --format [type]', 'Output format', 'recursive')
   .action((data1, data2) => console.log(genDiff(data1, data2, program.format)))
   .parse(process.argv);
-
-if (!process.argv.length) program.help();
