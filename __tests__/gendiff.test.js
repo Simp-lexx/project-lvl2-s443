3d90432d3760e1479f165832cc2f45c85164c4ba
Simp-lexx <imlexx007@gmail.com>
2019-03-08 21:20:42 +1000

    step 4 first release

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,14 +4,13 @@ import gendiff from '../src';
 
 const getFixturePath = fileName => path.join(__dirname, '__fixtures__', fileName);
 
-test('Test JSON differences', () => {
-  const getCompareResult = gendiff(getFixturePath('before.json'), getFixturePath('after.json'));
-  const getExpectResult = fs.readFileSync(getFixturePath('expectResult'), 'utf-8');
-  expect(getCompareResult).toBe(getExpectResult);
-});
-
-test('Test YAML differences', () => {
-  const getCompareResult = gendiff(getFixturePath('before.yml'), getFixturePath('after.yml'));
-  const getExpectResult = fs.readFileSync(getFixturePath('expectResult'), 'utf-8');
-  expect(getCompareResult).toBe(getExpectResult);
-});
+test.each([
+  ['before.json', 'after.json', 'expectResult'],
+  ['before.yml', 'after.yml', 'expectResult'],
+  ['before.ini', 'after.ini', 'expectResult'],
+])('.getDifference(%s, %s)',
+  (before, after, expected) => {
+    const getCompareResult = gendiff(getFixturePath(before), getFixturePath(after));
+    const getExpectResult = fs.readFileSync(getFixturePath(expected), 'utf-8');
+    expect(getCompareResult).toBe(getExpectResult);
+  });
--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,10 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const getParser = {
   '.json': JSON.parse,
   '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
 export default (ext, content) => getParser[ext](content);
